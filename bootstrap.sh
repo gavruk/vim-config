912f1dc175bfca83560d14abbe0808e8c14cbb75
Sergey Gavruk <svgavruk@gmail.com>
2016-01-19 21:15:19 +0300

    Update submodules + scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
-git submodule foreach git pull origin master
-cd ~/.vim/bundle/youcompleteme
+cd ~/.vim
 git submodule update --init --recursive
+ln ~/.vim/.vimrc ~/.vimrc
 cd ~/.vim/bundle/youcompleteme
 sh ./install.sh
 cd ~/.vim/bundle/tern
 npm install
+mkdir ~/.vim/undo ~/.vim/temp ~/.vim/backup
 cd ~/.vim
 
-echo "Successfully Updated"
+echo "Vim Config Successfully Installed"
